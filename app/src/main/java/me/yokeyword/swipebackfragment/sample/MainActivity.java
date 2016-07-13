e114cb1b42b184d0be1845c6b23292481996a631
zhangzhenzhong <zhangzhenzhong@le.com>
2016-07-13 11:00:28 +0800

    首次提交
    特性
    1、Activity内Fragment数大于1时，滑动返回的是Fragment，否则滑动返回的是Activity。
    2、支持左、右、左&右滑动（未来可能会增加更多滑动区域）
    3、支持Swipe时的滑动监听
    4、帮你处理了Fragment重叠的情况

---
+++ app/src/main/java/me/yokeyword/swipebackfragment/sample/MainActivity.java
@@ -0,0 +1,64 @@
+package me.yokeyword.swipebackfragment.sample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.widget.Toast;
+
+import me.yokeyword.swipebackfragment.SwipeBackActivity;
+import me.yokeyword.swipebackfragment.sample.fragment.FirstSwipeBackFragment;
+
+public class MainActivity extends SwipeBackActivity implements BaseSwipeBackFragment.OnAddFragmentListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            FirstSwipeBackFragment firstFragment = FirstSwipeBackFragment.newInstance();
+            loadFragment(firstFragment);
+        } else {
+            Toast.makeText(MainActivity.this, "啊哦,app被强杀喽~", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * 限制SwipeBack的条件,默认栈内Fragment数 <= 1时 , 优先滑动退出Activity , 而不是Fragment
+     *
+     * @return true: Activity可以滑动退出, 并且总是优先;  false: Activity不允许滑动退出
+     */
+    @Override
+    public boolean swipeBackPriority() {
+        return super.swipeBackPriority();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() == 1) {
+            finish();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void addFragment(Fragment fromFragment, Fragment toFragment) {
+        getSupportFragmentManager().beginTransaction()
+                .setCustomAnimations(R.anim.h_fragment_enter, R.anim.h_fragment_exit, R.anim.h_fragment_pop_enter, R.anim.h_fragment_pop_exit)
+                .add(R.id.fl_container, toFragment, toFragment.getClass().getSimpleName())
+                .hide(fromFragment)
+                .addToBackStack(toFragment.getClass().getSimpleName())
+                .commit();
+    }
+
+    private void loadFragment(Fragment toFragment) {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fl_container, toFragment, toFragment.getClass().getSimpleName())
+                .addToBackStack(toFragment.getClass().getSimpleName())
+                .commit();
+    }
+
+    @Override
+    public void onAddFragment(Fragment fromFragment, Fragment toFragment) {
+        addFragment(fromFragment, toFragment);
+    }
+}
---
+++ swipeback/src/main/java/me/yokeyword/swipebackfragment/SwipeBackFragment.java
@@ -0,0 +1,122 @@
+package me.yokeyword.swipebackfragment;
+
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+
+/**
+ * SwipeBackFragment
+ * Created by YoKeyword on 16/4/19.
+ */
+public class SwipeBackFragment extends Fragment {
+    private static final String SWIPEBACKFRAGMENT_STATE_SAVE_IS_HIDDEN = "SWIPEBACKFRAGMENT_STATE_SAVE_IS_HIDDEN";
+    private SwipeBackLayout mSwipeBackLayout;
+    private Animation mNoAnim;
+    boolean mLocking = false;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            boolean isSupportHidden = savedInstanceState.getBoolean(SWIPEBACKFRAGMENT_STATE_SAVE_IS_HIDDEN);
+
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            if (isSupportHidden) {
+                ft.hide(this);
+            } else {
+                ft.show(this);
+            }
+            ft.commit();
+        }
+
+        mNoAnim = AnimationUtils.loadAnimation(getActivity(), R.anim.no_anim);
+        onFragmentCreate();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(SWIPEBACKFRAGMENT_STATE_SAVE_IS_HIDDEN, isHidden());
+    }
+
+    private void onFragmentCreate() {
+        mSwipeBackLayout = new SwipeBackLayout(getActivity());
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        mSwipeBackLayout.setLayoutParams(params);
+        mSwipeBackLayout.setBackgroundColor(Color.TRANSPARENT);
+    }
+
+    protected View attachToSwipeBack(View view) {
+        mSwipeBackLayout.attachToFragment(this, view);
+        return mSwipeBackLayout;
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        if (hidden && mSwipeBackLayout != null) {
+            mSwipeBackLayout.hiddenFragment();
+        }
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        View view = getView();
+        initFragmentLayoutBg(view);
+        if (view != null) {
+            view.setClickable(true);
+        }
+    }
+
+    private void initFragmentLayoutBg(View view) {
+        if (view != null && !(view instanceof SwipeBackLayout) && view.getBackground() == null) {
+            int background = getWindowBackground();
+            view.setBackgroundResource(background);
+        } else {
+            if (view instanceof SwipeBackLayout) {
+                View childView = ((SwipeBackLayout) view).getChildAt(0);
+                if (childView != null && childView.getBackground() == null) {
+                    int background = getWindowBackground();
+                    childView.setBackgroundResource(background);
+                }
+            }
+        }
+    }
+
+    @Override
+    public Animation onCreateAnimation(int transit, boolean enter, int nextAnim) {
+        if (mLocking) {
+            return mNoAnim;
+        }
+        return super.onCreateAnimation(transit, enter, nextAnim);
+    }
+
+    protected int getWindowBackground() {
+        TypedArray a = getActivity().getTheme().obtainStyledAttributes(new int[]{
+                android.R.attr.windowBackground
+        });
+        int background = a.getResourceId(0, 0);
+        a.recycle();
+        return background;
+    }
+
+    public SwipeBackLayout getSwipeBackLayout() {
+        return mSwipeBackLayout;
+    }
+
+    public void setSwipeBackEnable(boolean enable) {
+        mSwipeBackLayout.setEnableGesture(enable);
+    }
+}
